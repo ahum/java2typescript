9801a14de485792275286edd4453462f45105830
Ed Eustace <ed.eustace@workday.com>
2025-02-26 21:39:43 +0000

    style: format Option.spec.ts with consistent indentation

--- tests/conversion/Option.spec.ts
+++ tests/conversion/Option.spec.ts
@@ -9,36 +9,41 @@ import * as path from "path";
 import { JavaToTypescriptConverter } from "../../src/conversion/JavaToTypeScript.js";
 
 describe("Options Tests", () => {
-    const testDir = path.join(process.cwd(), "tests");
-    const dataDir = path.join(testDir, "test-data");
-    const targetDir = path.join(testDir, "conversion", "generated");
+  const testDir = path.join(process.cwd(), "tests");
+  const dataDir = path.join(testDir, "test-data");
+  const targetDir = path.join(testDir, "conversion", "generated");
 
-    afterAll(async () => {
-        await fs.rm(targetDir, { recursive: true, force: true });
+  afterAll(async () => {
+    await fs.rm(targetDir, { recursive: true, force: true });
+  });
+
+  it("Prefix", async () => {
+    const rand = Math.random();
+
+    const converter = new JavaToTypescriptConverter({
+      packageRoot: path.join(dataDir, "java-sources", "org"),
+      outputPath: targetDir,
+      exclude: [],
+      include: [],
+      options: {
+        prefix: (sourcePath) => {
+          return `/**\n * ${rand}\n * Auto generated from ${sourcePath}\n * ${new Date().toString()} */`;
+        },
+        preferArrowFunctions: true,
+        convertAnnotations: false,
+        autoAddBraces: true,
+        addIndexFiles: true,
+      },
     });
+    await converter.startConversion();
 
-    it("Prefix", async () => {
-        const rand = Math.random();
-
-        const converter = new JavaToTypescriptConverter({
-            packageRoot: path.join(dataDir, "java-sources", "org"),
-            outputPath: targetDir,
-            exclude: [],
-            include: [],
-            options: {
-                prefix: (sourcePath) => {
-                    return `/**\n * ${rand}\n * Auto generated from ${sourcePath}\n * ${new Date().toString()} */`;
-                },
-                preferArrowFunctions: true,
-                convertAnnotations: false,
-                autoAddBraces: true,
-                addIndexFiles: true,
-            },
-        });
-        await converter.startConversion();
-
-        const targetFileName = path.join(targetDir, "java2typescript", "options", "OptionSpec.ts");
-        const targetFile = await fs.readFile(targetFileName, { encoding: "utf-8" });
-        expect(targetFile).toContain(`/**\n * ${rand}\n * Auto generated from`);
-    });
+    const targetFileName = path.join(
+      targetDir,
+      "java2typescript",
+      "options",
+      "OptionSpec.ts"
+    );
+    const targetFile = await fs.readFile(targetFileName, { encoding: "utf-8" });
+    expect(targetFile).toContain(`/**\n * ${rand}\n * Auto generated from`);
+  });
 });
